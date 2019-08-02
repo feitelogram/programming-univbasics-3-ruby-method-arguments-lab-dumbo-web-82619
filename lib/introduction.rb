cdcae9733d980b3ee9fe7bb88d9ecb2d27c185d4
Nicholas Feitel <nicholas.feitel@gmail.com>
2019-08-02 21:44:40 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,4 +7,4 @@ def introduction_with_language (name, language)
 end
 
 def introduction_with_language_optional (name, language)
-  puts 
+  puts "Hi, my name is #{name} and I am learning to program in #{language = Ruby}."
